a4fbd1231b32dd66fa1e4a2cdcbd0ec91c63e953
sam1code <samcse95@gmail.com>
2021-09-29 14:22:16 +0530

    added

--- scripts/mob.js
+++ scripts/mob.js
@@ -1,12 +1,19 @@
-const slide = document.querySelector(".maincontent");
-const divs = document.querySelectorAll(".pages");
+const btn1 = document.querySelector(".btn1");
+const btn2 = document.querySelector(".btn2");
+const btn3 = document.querySelector(".btn3");
+const btn4 = document.querySelector(".btn4");
+const btn5 = document.querySelector(".btn5");
+const btn6 = document.querySelector(".btn6");
+const btn7 = document.querySelector(".btn7");
 
-const yobro = document.querySelector(".yobro");
-
-let counter = 1;
-const size = window.innerWidth;
-
-slide.style.transfrom = 'translateX("-size* counter)+ "px)';
-yobro.addEventListener("click", () => {
-  slide.style.transition = "transform 0.4s ease-in-out";
-});
+for (i = 1; i <= 1000; i++) {
+  if (i % 7 === 1) {
+    btn1.click();
+    continue;
+  }
+  setTimeout(() => {
+    if (i % 2 === 1) {
+      btn1.click();
+    }
+  }, 3000);
+}
